0a873a6cda47749d7f9470fd1348f9d9a90fb3ef
Jim Wheeler <jwheele1@gmail.com>
2022-02-17 14:38:25 -0600

    Added conftest

---
+++ conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium install webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+@pytest.fixture(scope="class")
+def setup():
+    ser = Service('C:\WebDriver\Bin\chromedriver.exe')
+    op = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=ser, options=op)
+    driver.get("https://rahulshettyacademy.com/angularpractice/")
+    driver.maximize_window()
--- test_e2e.py
+++ test_e2e.py
@@ -4,6 +4,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.mark.usefixtures("setup")
 class TestOne:
 
     def test_e2e(self):
+        ser = Service('C:\WebDriver\Bin\chromedriver.exe')
+        op = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=ser, options=op)
+        driver.maximize_window()
