7042bd0978c06ec03b144304d4b8e341bc5cb1f1
Jim Wheeler <jwheele1@gmail.com>
2022-02-17 14:31:01 -0600

    Import Selenium

---
+++ test_e2e.py
@@ -0,0 +1,9 @@
+from selenium install webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestOne:
+
+    def test_e2e(self):
