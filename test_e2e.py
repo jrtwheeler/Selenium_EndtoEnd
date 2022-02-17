5dbcb332570077662facfdc4bd526f210d59efaa
Jim Wheeler <jwheele1@gmail.com>
2022-02-17 14:57:59 -0600

    Added BaseClass

--- test_e2e.py
+++ test_e2e.py
@@ -1,11 +1,11 @@
 from selenium install webdriver
+from Selenium_EndtoEnd.utilities.BaseClass import BaseClass
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.mark.usefixtures("setup")
-class TestOne:
+class TestOne(BaseClass):
 
     def test_e2e(self, setup):
 
